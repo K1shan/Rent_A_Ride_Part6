c3477bbb1409e728835f75d4797362fe7f19dbfb
Wayne Kung <waynekung@outlook.com>
2017-10-25 19:40:28 -0400

    Implemented store for ParamsManager

--- src/edu/uga/cs/rentaride/persistence/impl/RentARideParamsManager.java
+++ src/edu/uga/cs/rentaride/persistence/impl/RentARideParamsManager.java
@@ -39,12 +39,65 @@ public class RentARideParamsManager {
 		this.objectLayer = objectLayer;
 	}//constructor
 	
+	@SuppressWarnings("resource")
+	public void store( RentARideParams rentARideConfig ) throws RARException{
+    	
+		boolean persist = false;
+
+		String paramsInsertQuery = 
+		"INSERT INTO RENT_A_RIDE_PARAMS "
+		+ "(member_fee, late_fee)"
+
+		+ "VALUES "
+		+ "( ?, ?)";
+
+		String paramsUpdateQuery =
+		"UPDATE RENT_A_RIDE_PARAMS "
+			+ "member_fee=?, late_fee=? ";
+
+
+		PreparedStatement	pstmt;
+		int					inscnt;
+		long				paramsId;
+
+
+		try{
+			if( !rentARideConfig.isPersistent()) {
+				pstmt = (PreparedStatement) con.prepareStatement(paramsInsertQuery);
+			}
+			else{
+				pstmt = (PreparedStatement) con.prepareStatement(paramsUpdateQuery);
+			}
+
+			if(rentARideConfig.getMembershipPrice() >= 0 ) {
+				pstmt.setLong(1, rentARideConfig.getMembershipPrice());
+			}
+			else {
+				throw new RARException("RentARideParamsManager.save: can't save a params: member_fee undefined");
+			}
+
+			if(rentARideConfig.getLateFee() >= 0 ) {
+				pstmt.setLong(2, rentARideConfig.getLateFee());
+			}
+			else {
+				throw new RARException("RentARideParamsManager.save: can't save a params: late_fee undefined");
+			}
+
+			pstmt.executeUpdate();
+			
+		} //try
+		catch(SQLException e) {
+			e.printStackTrace();
+			throw new RARException( "RentARideParamsManager.save: failed to save a params: " + e );
+		}
+
+    }
+	
+
 	public RentARideParams restore() throws RARException{
 		// TODO
 		return null;
 	}
 	
-    public void store( RentARideParams rentARideConfig ) throws RARException{
-    	// TODO
-    }
+    
 }
